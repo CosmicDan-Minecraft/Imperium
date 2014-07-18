c1c04b2d5fa97688a387dbabd41d1af24c10d833
CosmicDan <dconnolly.au@gmail.com>
2014-07-18 11:48:43 +1000

    Player properties packet - it's bidirectional!

--- src/main/java/com/cosmicdan/minecraftempires/server/PacketHandler.java
+++ src/main/java/com/cosmicdan/minecraftempires/server/PacketHandler.java
@@ -8,7 +8,9 @@ public class PacketHandler {
     public static final SimpleNetworkWrapper packetReq = NetworkRegistry.INSTANCE.newSimpleChannel("minecraftempires");
 
     public static void init() {
-        // Register EntityPlayerME updates for clients (i.e. events/quests information)
+        // Register EntityPlayerME for updates TO clients (e.g. status and information for player GUI's)
         packetReq.registerMessage(SyncPlayerME.class, SyncPlayerME.class, 0, Side.CLIENT);
+        // Register EntityPlayerME for updates FROM clients (e.g. events and quests triggered)
+        packetReq.registerMessage(SyncPlayerME.class, SyncPlayerME.class, 1, Side.SERVER);
     }
 }
--- src/main/java/com/cosmicdan/minecraftempires/server/SyncPlayerME.java
+++ src/main/java/com/cosmicdan/minecraftempires/server/SyncPlayerME.java
@@ -1,23 +1,29 @@
 package com.cosmicdan.minecraftempires.server;
 
-import java.util.ArrayList;
+import io.netty.buffer.ByteBuf;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 
 import com.cosmicdan.minecraftempires.Main;
+import com.cosmicdan.minecraftempires.eventhandlers.WorldTickEvents;
 import com.cosmicdan.minecraftempires.medata.player.MinecraftEmpiresPlayer;
 
-import io.netty.buffer.ByteBuf;
-import cpw.mods.fml.client.FMLClientHandler;
 import cpw.mods.fml.common.network.ByteBufUtils;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
 import cpw.mods.fml.relauncher.Side;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
 
+/*
+ * this network packet is responsible for sending
+ * player properties between server and client
+ * It's bidirectional!
+ */
 public class SyncPlayerME implements IMessage, IMessageHandler<SyncPlayerME, IMessage> {
 
     public String eventListDone;
+    public String eventListPending;
+    public String eventListPendingInstant;
     
     public SyncPlayerME() {
         
@@ -26,32 +32,51 @@ public class SyncPlayerME implements IMessage, IMessageHandler<SyncPlayerME, IMe
     public SyncPlayerME(EntityPlayer player) {
         MinecraftEmpiresPlayer playerME = MinecraftEmpiresPlayer.get(player);
         eventListDone = playerME.eventListDone.toString();
+        eventListPending = playerME.eventListPending.toString();
+        eventListPendingInstant = playerME.eventListPendingInstant.toString();
     }
 
     // fromBytes loads the data into the class ready for sending
     @Override
     public void fromBytes(ByteBuf buf) {
         eventListDone = ByteBufUtils.readUTF8String(buf);
+        eventListPending = ByteBufUtils.readUTF8String(buf);
+        eventListPendingInstant = ByteBufUtils.readUTF8String(buf);
     }
 
     // toBytes is for sending a packet/request (in this case, server to the client)
     @Override
     public void toBytes(ByteBuf buf) {
         ByteBufUtils.writeUTF8String(buf, this.eventListDone);
+        ByteBufUtils.writeUTF8String(buf, this.eventListPending);
+        ByteBufUtils.writeUTF8String(buf, this.eventListPendingInstant);
     }
 
     // onMessage is called after the data is received i.e. we "assign" the packet/message data here
     @Override
     public IMessage onMessage(SyncPlayerME msg, MessageContext ctx) {
-        if (ctx.side == Side.CLIENT) {
             EntityPlayer player = Main.proxy.getPlayerFromMessageContext(ctx);
             if ( player != null) {
                 MinecraftEmpiresPlayer playerME = MinecraftEmpiresPlayer.get(player);
                 if (playerME != null ) {
-                    playerME.eventListDone = MinecraftEmpiresPlayer.stringToArrayList(msg.eventListDone);
+                    if (ctx.side == Side.CLIENT) {
+                        // we only want to update eventsDone for the client (i.e. server sending to players)
+                        playerME.eventListDone = MinecraftEmpiresPlayer.stringToArrayList(msg.eventListDone);
+                    } else {
+                        // we only want to update pending events for the server (i.e. client needs to update server)
+                        playerME.eventListPending = MinecraftEmpiresPlayer.stringToArrayList(msg.eventListPending);
+                        playerME.eventListPendingInstant = MinecraftEmpiresPlayer.stringToArrayList(msg.eventListPendingInstant);
+                        if (msg.eventListPendingInstant.length() > 2) {
+                            // found pending instant event, notify WorldTickEvents immediately...
+                            // UNLESS this player is already pending!
+                            if (!WorldTickEvents.isPlayerPendingInstant((EntityPlayerMP)player)) {
+                                WorldTickEvents.addPlayerToPendingInstants((EntityPlayerMP)player);
+                                WorldTickEvents.eventPendingInstant = true;
+                            }
+                        }
+                    }
                 }
             }
-        }
         return null;
     }
 }
