32888c1a12ceea106d93d5d8bd2db61b7a74ff0e
CosmicDan <dconnolly.au@gmail.com>
2014-07-20 20:47:48 +1000

    EntityEvents - also prevent IMob spawns (Slimes everywhere!), tiny cleanup

--- src/main/java/com/cosmicdan/minecraftempires/eventhandlers/EntityEvents.java
+++ src/main/java/com/cosmicdan/minecraftempires/eventhandlers/EntityEvents.java
@@ -1,11 +1,12 @@
 package com.cosmicdan.minecraftempires.eventhandlers;
 
 import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.monster.IMob;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraftforge.event.entity.EntityEvent.EntityConstructing;
 import net.minecraftforge.event.entity.living.LivingSpawnEvent.CheckSpawn;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
+import net.minecraftforge.event.entity.EntityEvent.EntityConstructing;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 
 import com.cosmicdan.minecraftempires.items.ModItems;
@@ -28,7 +29,6 @@ public class EntityEvents {
     }
     
     public void onEntityJoin(EntityJoinWorldEvent event) {
-        
         // TODO: Is this needed for multiplayer? I think PlayerEvents should handle this fine, need to test
         //if(!event.entity.worldObj.isRemote && event.entity instanceof EntityPlayer) {
         //    PacketHandler.packetReq.sendTo(new SyncPlayerME((EntityPlayer)event.entity), (EntityPlayerMP)event.entity);
@@ -36,7 +36,7 @@ public class EntityEvents {
     }
     
     @SubscribeEvent
-    public void onPlayerPickup(EntityItemPickupEvent event) {
+    public void onEntityPickup(EntityItemPickupEvent event) {
         if (event.entity instanceof EntityPlayer) {
             MinecraftEmpiresPlayer playerME = MinecraftEmpiresPlayer.get((EntityPlayer) event.entity);
             if (event.item.getEntityItem().getItem() == ModItems.brushwood) {
@@ -51,14 +51,14 @@ public class EntityEvents {
     
     @SubscribeEvent
     public void onCheckSpawn(CheckSpawn event) {
-        // always allow players, not sure it's needed but whatever
-        if (event.entity instanceof EntityPlayer) {
+        if(!event.entity.worldObj.isRemote) {
+            if (event.entity instanceof EntityMob || event.entity instanceof IMob) {
+                event.setResult(Result.DENY);
+                return;
+            }
             event.setResult(Result.ALLOW);
-            return;
-        }
-        if (event.entity instanceof EntityMob) {
-            event.setResult(Result.DENY);
-            return;
         }
     }
+    
+    
 }
