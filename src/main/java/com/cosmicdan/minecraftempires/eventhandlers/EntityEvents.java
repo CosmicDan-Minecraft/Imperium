3b8ea6f727272ead20483b38a13a3da0020ae227
CosmicDan <dconnolly.au@gmail.com>
2014-07-20 20:49:58 +1000

    All animals flee from the player (CHASING SHEEP IS FUN!)

--- src/main/java/com/cosmicdan/minecraftempires/eventhandlers/EntityEvents.java
+++ src/main/java/com/cosmicdan/minecraftempires/eventhandlers/EntityEvents.java
@@ -1,7 +1,9 @@
 package com.cosmicdan.minecraftempires.eventhandlers;
 
+import net.minecraft.entity.ai.EntityAIAvoidEntity;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.monster.IMob;
+import net.minecraft.entity.passive.EntityAnimal;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraftforge.event.entity.living.LivingSpawnEvent.CheckSpawn;
@@ -60,5 +62,22 @@ public class EntityEvents {
         }
     }
     
-    
+    @SubscribeEvent
+    public void onEntityJoinWorld(EntityJoinWorldEvent event) {
+        if(!event.entity.worldObj.isRemote) {
+            if (event.entity instanceof EntityAnimal) {
+                // make animals run away from the player
+                EntityAnimal entityAnimal = (EntityAnimal) event.entity;
+                if (entityAnimal.tasks.taskEntries.size() > 0) { // ensure it actually uses the new AI, just in case
+                    /*
+                     *  - First int of addTask seems to be priority
+                     *  theEntity, targetEntityClass, distanceFromEntity, farSpeed, nearSpeed
+                     *  - distanceFromEntity - no idea how this is measured but it seems to be close to block distance
+                     *  - farSpeed and nearSpeed are multipliers of their base speed
+                     */
+                    entityAnimal.tasks.addTask(1, new EntityAIAvoidEntity(entityAnimal, EntityPlayer.class, 6.0F, 1.5D, 2.0D));
+                }
+            }
+        }
+    }
 }
